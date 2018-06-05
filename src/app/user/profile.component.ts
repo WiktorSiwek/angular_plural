5dc7918aebc761a4d204274b1cfd922175240362
Wiktor Siwek <wiktor.siwek@gft.com>
2018-06-05 09:37:22 +0200

    Created reactife form for profile component

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
-       
+export class ProfileComponent implements OnInit {
+  profileForm:FormGroup
+
+  constructor(private _authService:AuthService, private _router:Router) {}
+
+      ngOnInit() {
+        let firstName = new FormControl(this._authService.currentUser.firstName);
+        let lastName = new FormControl(this._authService.currentUser.lastName);
+        this.profileForm = new FormGroup({
+          firstName,
+          lastName
+        })
+      }
+
+      cancel() {
+        this._router.navigate(['/events'])
+      }
+
+      saveProfile(formValues) {
+        this._authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+        this._router.navigate(['/events'])
+      }
 }
