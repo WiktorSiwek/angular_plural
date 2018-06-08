f4d84a48f6cd8fbc4a686541d5e26ed0042a3b91
Wiktor Siwek <wiktor.siwek@gft.com>
2018-06-08 15:53:55 +0200

    Implemented persisting authentications status across pae refreshes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _auth: AuthService) {}
+
+  ngOnInit() {
+    this._auth.checkAuthenticationStatus();
+  }
 }
--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -8,12 +8,12 @@ import { of } from 'rxjs/observable/of';
 export class AuthService {
   currentUser: IUser;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   loginUser(userName: string, password: string) {
 
     const loginInfo = { username: userName, password: password };
-    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
     return this.http.post('/api/login', loginInfo, options)
       .pipe(tap(data => {
@@ -30,19 +30,19 @@ export class AuthService {
 
   checkAuthenticationStatus() {
     this.http.get('/api/currentIdentity')
-    .pipe(tap(data => {
-      if (data instanceof Object) {
-        this.currentUser = <IUser>data;
-      }
-    }))
-    .subscribe();
+      .pipe(tap(data => {
+        if (data instanceof Object) {
+          this.currentUser = <IUser>data;
+        }
+      }))
+      .subscribe();
   }
 
   updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
 
-    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
     return this.http.put(`/api/users/${this.currentUser.id}`, this.currentUser, options);
   }
@@ -50,7 +50,7 @@ export class AuthService {
   logout() {
     this.currentUser = undefined;
 
-    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
     return this.http.post('/api/logout', {}, options);
   }
 }
